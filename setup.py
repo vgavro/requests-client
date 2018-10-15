163672c3acfebb997af99dc25fcd73bc1e4cef28
Victor Gavro <vgavro@gmail.com>
2018-10-15 13:20:22 +0300

    updated to 0.0.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires = [
 
 setup(
     name='requests-client',
-    version='0.0.3',
+    version='0.0.4',
     description='http://github.com/vgavro/requests-client',
     long_description='http://github.com/vgavro/requests-client',
     license='BSD',
