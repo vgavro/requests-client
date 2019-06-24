abb929cba5191f3650f950ba80858eb3e143e754
Victor Gavro <vgavro@gmail.com>
2019-06-24 18:11:22 +0300

    fixes for marshmallow>=3.0.0rc7

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires = [
 
 setup(
     name='requests-client',
-    version='0.0.13',
+    version='0.0.14',
     description='http://github.com/vgavro/requests-client',
     long_description='http://github.com/vgavro/requests-client',
     license='BSD',
